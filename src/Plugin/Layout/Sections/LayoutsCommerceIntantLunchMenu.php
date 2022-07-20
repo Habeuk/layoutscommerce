01aa448d9fba36c95e5984ad6ffc81f1d3e662e3
kouwa stephane <kksasteph888@gmail.com>
2022-07-20 23:54:40 +0200

    MAJ

--- src/Plugin/Layout/Sections/LayoutsCommerceIntantLunchMenu.php
+++ src/Plugin/Layout/Sections/LayoutsCommerceIntantLunchMenu.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Drupal\layoutscommerce\Plugin\Layout\Sections;
 
 use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
@@ -8,8 +7,9 @@ use Drupal\formatage_models\FormatageModelsThemes;
 use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
 
 /**
- * 
+ *
  * Custom layout for Layoutscommmerce module
+ *
  * @Layout(
  *   id = "instant_lunch_menu",
  *   label = @Translation(" Commerce Instant Lunch Menu "),
@@ -20,74 +20,74 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *   default_region = "i_l_menu_title",
  *   regions = {
  *      "i_l_menu_title" = {
- *          "label" = @Translation("i_l_menu_title "),     
+ *          "label" = @Translation("i_l_menu_title "),
  *      },
  *      "i_l_menu_description" = {
- *          "label" = @Translation("i_l_menu_description"),     
+ *          "label" = @Translation("i_l_menu_description"),
  *      },
  *      "i_l_menu_image" = {
- *          "label" = @Translation("i_l_menu_image "),     
+ *          "label" = @Translation("i_l_menu_image "),
  *      },
  *      "i_l_menu_date_text" = {
- *          "label" = @Translation("i_l_menu_date_text "),     
+ *          "label" = @Translation("i_l_menu_date_text "),
  *      },
  *      "i_l_menu_date" = {
- *          "label" = @Translation("i_l_menu_date "),     
+ *          "label" = @Translation("i_l_menu_date "),
  *      }
  *   }
  * )
- * 
+ *
  */
-class LayoutsCommerceIntantLunchMenu extends FormatageModelsSection 
-{
-    /**
-    *
-    * {@inheritdoc}
-    * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
-    */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
-        // TODO Auto-generated method stub
-        parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
-        $this->pluginDefinition->set('icon', drupal_get_path('module', 'layoutscommerce') . "/icones/sections/layoutscommerce_instant_lunch_menu.png");
-    }
+class LayoutsCommerceIntantLunchMenu extends FormatageModelsSection {
   
-    /**
-    *
-    * {@inheritdoc}
-    * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build()
-    */
-    public function build(array $regions) {
-        // TODO Auto-generated method stub
-        $build = parent::build($regions);
-        FormatageModelsThemes::formatSettingValues($build); 
-        return $build;
-    }
-
-    /**
-     * 
-     * {@inheritdoc}
-     * 
-     */
-    public function defaultConfiguration() {
-        return parent::defaultConfiguration() + [
-            'load_libray' => false,
-            'infos' => [
-                'builder-form' => true,
-                'info' => [
-                    'title' => 'Texte information',
-                    'loader' => 'static'
-                ],
-                'fields' => [
-                    'i_l_menu_title' => [
-                        'text_html' => [
-                            'label' => 'Titre',
-                            'value' => '<span class="il-colored">Traiteur plateaux repas</span> pour entreprises'
-                        ]
-                    ],
-                    'i_l_menu_description' => [
-                        'text_html' => [
-                            'label' => 'Description',
-                            'value' => 'C’est la fin de la livraison traditionnelle du plateau repas froid en entreprise, avec
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
+    // TODO Auto-generated method stub
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+    $this->pluginDefinition->set('icon', drupal_get_path('module', 'layoutscommerce') . "/icones/sections/layoutscommerce_instant_lunch_menu.png");
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build()
+   */
+  public function build(array $regions) {
+    // TODO Auto-generated method stub
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    return $build;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   *
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'load_libray' => false,
+      'infos' => [
+        'builder-form' => true,
+        'info' => [
+          'title' => 'Texte information',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'i_l_menu_title' => [
+            'text_html' => [
+              'label' => 'Titre',
+              'value' => '<span class="il-colored">Traiteur plateaux repas</span> pour entreprises'
+            ]
+          ],
+          'i_l_menu_description' => [
+            'text_html' => [
+              'label' => 'Description',
+              'value' => 'C’est la fin de la livraison traditionnelle du plateau repas froid en entreprise, avec
                                     Instant-Lunch,
                                     faites vous livrer
                                     un plateau repas chaud pour vous et vos collaborateurs.<strong>Nous vous proposons un menu
@@ -105,27 +105,28 @@ class LayoutsCommerceIntantLunchMenu extends FormatageModelsSection
                                     Découvrez et profitez dès maintenant du menu qui vous ressemble le plus et qui saura séduire vos
                                     papilles avec notre
                                     offre traiteur plateaux repas…'
-                        ]
-                    ],
-                    'i_l_menu_image' => [
-                        'img_bg' => [
-                            'label' => 'BG img for menu'
-                        ]
-                    ],
-                    'i_l_menu_date_text' => [
-                        'text_html' => [
-                            'label' => 'Date text',
-                            'value' => 'Retrouvez nos menus de la semaine ci-dessous : '
-                        ]
-                    ],
-                    'i_l_menu_date' => [
-                        'text_html' => [
-                            'label' => 'Date',
-                            'value' => 'du 18 au 24 juillet'
-                        ]
-                    ]
-                ]
-            ],
-        ];
-    }
+            ]
+          ],
+          'i_l_menu_image' => [
+            'img_bg' => [
+              'label' => 'BG img for menu'
+            ]
+          ],
+          'i_l_menu_date_text' => [
+            'text_html' => [
+              'label' => 'Date text',
+              'value' => 'Retrouvez nos menus de la semaine ci-dessous : '
+            ]
+          ],
+          'i_l_menu_date' => [
+            'text_html' => [
+              'label' => 'Date',
+              'value' => 'du 18 au 24 juillet'
+            ]
+          ]
+        ]
+      ]
+    ];
+  }
+  
 }
