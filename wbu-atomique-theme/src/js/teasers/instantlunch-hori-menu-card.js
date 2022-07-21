62a0058b9f424d2e77f4ad8c26ab8290980795a6
gugledas <azabzistany@gmail.com>
2022-07-21 16:09:04 +0100

    maj

--- wbu-atomique-theme/src/js/teasers/instantlunch-hori-menu-card.js
+++ wbu-atomique-theme/src/js/teasers/instantlunch-hori-menu-card.js
@@ -1,2 +1,2 @@
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/instant-lunch/molecule/il-hori-menu-card.scss";
-import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/instant-lunch/js/il-menu-card.js";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/instant-lunch/js/il-action-card.js";
--- wbu-atomique-theme/src/js/teasers/instantlunch-menu-card.js
+++ wbu-atomique-theme/src/js/teasers/instantlunch-menu-card.js
@@ -1,2 +1,2 @@
-import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/instant-lunch/js/il-menu-card.js";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/instant-lunch/js/il-action-card.js";
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/instant-lunch/molecule/il-menu-card.scss";
